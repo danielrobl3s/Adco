c06c34eda81926047b15a54ae609125dcc49624e
danielrobl3s <danielrobles200217@gmail.com>
2023-03-22 23:47:52 -0600

    API REST django Login system terminado

--- adco/login/views.py
+++ adco/login/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Usuario
+from rest_framework import generics
+from .serializers import UsuarioSerializer
 
-# Create your views here.
+
+class UsuarioCreate(generics.CreateAPIView):
+    # API endpoint that allows creation of a new user
+    queryset = Usuario.objects.all(),
+    serializer_class = UsuarioSerializer
+
+
+class UsuarioList(generics.ListAPIView):
+    # API endpoint that allows user to be viewed.
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+    
+class UsuarioDetail(generics.RetrieveAPIView):
+    # API endpoint that returns a single customer by pk.
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+    
+class UsuarioUpdate(generics.RetrieveUpdateAPIView):
+    # API endpoint that allows a customer record to be updated.
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class UsuarioDelete(generics.RetrieveDestroyAPIView):
+    # API endpoint that allows a customer record to be deleted.
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
