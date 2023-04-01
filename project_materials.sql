d7357f382b705730d8e3fcf6e9d3805b3974e1ff
Mario Alvarado <mario.alvarado.isw@gmail.com>
2023-03-31 22:25:38 -0600

    Add: Project_materials Foreign key

--- project_materials.sql
+++ project_materials.sql
@@ -10,7 +10,11 @@ CREATE TABLE IF NOT EXISTS public.project_materials
     cantidad double precision,
     concepto character varying(20) COLLATE pg_catalog."default",
     fecha date,
-    CONSTRAINT project_materials_pkey PRIMARY KEY (id_material)
+    CONSTRAINT project_materials_pkey PRIMARY KEY (id_material),
+    CONSTRAINT fk_proyecto_materials FOREIGN KEY (id_proyecto)
+        REFERENCES public.project_clientes (id_proyecto) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;
