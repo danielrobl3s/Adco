cabe6a33e4d8cd408d5a67c82a147a85c06dd84a
Mario Alvarado <mario.alvarado.isw@gmail.com>
2023-03-31 22:24:47 -0600

    Add: Project_gastos Foreign key

--- project_gastos.sql
+++ project_gastos.sql
@@ -6,12 +6,16 @@ CREATE TABLE IF NOT EXISTS public.project_gastos
 (
     id_gasto integer NOT NULL,
     id_proyecto integer NOT NULL,
-	concepto character varying(20) COLLATE pg_catalog."default",
+    concepto character varying(20) COLLATE pg_catalog."default",
     monto double precision,
-	metodo_pago character varying(20) COLLATE pg_catalog."default",
-	cobrador character varying(20) COLLATE pg_catalog."default",
+    metodo_pago character varying(20) COLLATE pg_catalog."default",
+    cobrador character varying(20) COLLATE pg_catalog."default",
     fecha date,
-    CONSTRAINT project_gastos_pkey PRIMARY KEY (id_gasto)
+    CONSTRAINT project_gastos_pkey PRIMARY KEY (id_gasto),
+    CONSTRAINT fk_proyecto_gastos FOREIGN KEY (id_proyecto)
+        REFERENCES public.project_clientes (id_proyecto) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;
