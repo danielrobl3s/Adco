4582f988d2521620c663397f26fe355f02a1c953
Mario Alvarado <mario.alvarado.isw@gmail.com>
2023-03-31 22:02:41 -0600

    Add project_clientes table

---
+++ project_clientes.sql
@@ -0,0 +1,20 @@
+-- Table: public.project_clientes
+
+-- DROP TABLE IF EXISTS public.project_clientes;
+
+CREATE TABLE IF NOT EXISTS public.project_clientes
+(
+    id_cliente integer NOT NULL,
+    id_proyecto integer NOT NULL,
+    nombre character varying(50) COLLATE pg_catalog."default",
+    metodo_pago character varying(20) COLLATE pg_catalog."default",
+    fecha date,
+    documentos character varying(100) COLLATE pg_catalog."default",
+    observaciones character varying(100) COLLATE pg_catalog."default",
+    CONSTRAINT project_clientes_pkey PRIMARY KEY (id_cliente)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.project_clientes
+    OWNER to postgres;
