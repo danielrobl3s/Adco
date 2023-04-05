309b17f5f0b69839bb87864bf265648bd1469865
danielrobl3s <danielrobles200217@gmail.com>
2023-04-05 14:09:32 -0600

    CRUD operations without CSRF Token

--- rest_auth/clients/urls.py
+++ rest_auth/clients/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ClientesList.as_view(), name='clientes-list'),
-    #path('/<pk:int>', ClientesDetail.as_view(), name='detalles-cliente') #<------redirecciona a los prospectos donde esta la info de los clientes
+    path('<pk>/', views.ClientesDetail.as_view(), name='detalles-cliente') #<------redirecciona a los prospectos donde esta la info de los clientes
 ]
--- rest_auth/clients/views.py
+++ rest_auth/clients/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework import generics, permissions
 from .models import Clientes
 from .serializers import ClientesSerializer
@@ -11,7 +12,11 @@ class ClientesList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
-        
+
+    @ensure_csrf_cookie
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
 class ClientesDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ClientesSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -19,3 +24,14 @@ class ClientesDetail(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return Clientes.objects.filter(created_by=self.request.user)
 
+    @ensure_csrf_cookie
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @ensure_csrf_cookie
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    @ensure_csrf_cookie
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
