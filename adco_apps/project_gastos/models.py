aba64ace2b961073081939b411c3833eaf4e1802
Mario Alvarado <mario.alvarado.isw@gmail.com>
2023-04-11 20:50:35 -0600

    No habia borrado los comentarios jaja

--- adco_apps/project_gastos/models.py
+++ adco_apps/project_gastos/models.py
@@ -1,22 +1,5 @@
 from django.db import models
 from project_clientes.models import ProjectClientes
-
-# class ProjectGastos(models.Model):
-#     id_gasto = models.AutoField(primary_key=True)
-#     id_proyecto = models.AutoField(unique=True)
-#     concepto = models.CharField(max_length=20, blank=True, null=True)
-#     monto = models.FloatField(blank=True, null=True)
-#     metodo_pago = models.CharField(max_length=20, blank=True, null=True)
-#     cobrador = models.CharField(max_length=20, blank=True, null=True)
-#     fecha = models.DateField(blank=True, null=True)
-
-#     class Meta:
-
-#         db_table = 'project_gastos'
-
-#         def __str__(self) -> str:
-#             return self.title
-
 class ProjectGastos(models.Model):
     id_gasto = models.AutoField(primary_key=True)
     id_proyecto = models.ForeignKey(ProjectClientes, on_delete=models.CASCADE, default=0)
--- adco_apps/project_materials/models.py
+++ adco_apps/project_materials/models.py
@@ -1,20 +1,5 @@
 from django.db import models
 from project_clientes.models import ProjectClientes
-
-# class ProjectMaterials(models.Model):
-#     id_material = models.AutoField(primary_key=True)
-#     id_proyecto = models.AutoField(unique=True)
-#     recurso = models.CharField(max_length=20, blank=True, null=True)
-#     cantidad = models.FloatField(blank=True, null=True)
-#     concepto = models.CharField(max_length=20, blank=True, null=True)
-#     fecha = models.DateField(blank=True, null=True)
-
-#     class Meta:
-
-#         db_table = 'project_materials'
-#         def __str__(self) -> str:
-#             return self.title
-
 class ProjectMaterials(models.Model):
     id_material = models.AutoField(primary_key=True)
     id_proyecto = models.ForeignKey(ProjectClientes, on_delete=models.CASCADE, default=0)
