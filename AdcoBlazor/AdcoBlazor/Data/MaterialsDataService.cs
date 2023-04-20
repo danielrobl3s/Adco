2832415c0612668e5fcebd9379c65811d0f8ca37
filybox <meno117@outlook.com>
2023-04-19 21:54:30 -0600

    Menu y place holders

---
+++ AdcoBlazor/AdcoBlazor/Data/MaterialsDataService.cs
@@ -0,0 +1,38 @@
+﻿namespace AdcoBlazor.Data;
+
+public class MaterialsDataService
+{
+    private static readonly string[] Observaciones = new[]
+    {
+        "si", "no", "la", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private static readonly string[] Concepto = new[]
+    {
+        "pago de materiales", "pago proveedores"
+    };
+
+    private static readonly string[] MetodoPago = new[]
+{
+        "Tarjeta", "Efectivo"
+    };
+
+    private static readonly string[] Documentos = new[]
+    {
+        "www.drive.com", "www.youtubr.com"
+    };
+
+    public Task<MaterialsData[]> GetMaterialsAsync(DateTime startDate)
+    {
+        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new MaterialsData
+		{
+            Id_gasto  = Random.Shared.Next(1, 55),
+            Concepto = Concepto[Random.Shared.Next(Concepto.Length)],
+			Monto = Random.Shared.Next(2000, 10000),
+			MetodoPago = MetodoPago[Random.Shared.Next(MetodoPago.Length)],
+            Date = startDate.AddDays(index)
+
+		}).ToArray());
+    }
+}
+
---
+++ AdcoBlazor/AdcoBlazor/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using AdcoBlazor.Data;
+
+namespace AdcoBlazor;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+        builder.Services.AddSingleton<ClientesDataService>();
+
+		builder.Services.AddSingleton<MaterialsDataService>();
+
+		return builder.Build();
+	}
+}
