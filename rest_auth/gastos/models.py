1ddecd85a6b789dcf8cf67f2d3097827e36d54ef
danielrobl3s <danielrobles200217@gmail.com>
2023-04-25 20:45:12 -0600

    proveedores y trabajadores and gastos tables altered

--- rest_auth/gastos/models.py
+++ rest_auth/gastos/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from proyectos.models import Proyecto
 from django.contrib.auth.models import User
+from trabajadores_proveedores.models import TrabajadoresProveedores
 
 class Gastos(models.Model):
     concepto = models.CharField(max_length=50)
     monto = models.FloatField()
     metodo_pago = models.CharField(max_length=50)
-    cobrador = models.CharField(max_length=50)
+    cobrador = models.ForeignKey(TrabajadoresProveedores, on_delete=models.CASCADE)
     fecha = models.DateField()
     proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE, null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
